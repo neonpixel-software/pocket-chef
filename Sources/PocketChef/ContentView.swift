c6f8c1f99a14d857f2d432421cd115d84f2cc3c4
adam-moto <nick@neonpixel.eu>
2026-09-14 14:23:30 +0200

    Phase 1.1: scaffold multiplatform SwiftUI app (iOS, iPadOS, macOS)

---
+++ Sources/PocketChef/ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Pocket Chef")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tests/PocketChefTests/PocketChefTests.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+final class PocketChefTests: XCTestCase {
+    func testPlaceholder() {
+        XCTAssertTrue(true)
+    }
+}
